3f7ae5554f09ad2dcd4a5879d88cd8fc0f33e998
Crategus <kaiser@crategus.com>
2023-01-17 22:58:23 +0100

    Work on the testsuite

--- test/rtest-pango-cairo-rendering.lisp
+++ test/rtest-pango-cairo-rendering.lisp
@@ -18,8 +18,7 @@
           (g:gtype (cffi:foreign-funcall "pango_cairo_font_get_type" :size))))
   ;; Get the names of the interface properties.
   (is (equal '()
-             (mapcar #'g:param-spec-name
-                     (g:object-interface-list-properties "PangoCairoFont"))))
+             (list-interface-properties "PangoCairoFont")))
   ;; Get the interface definition
   (is (equal '(DEFINE-G-INTERFACE "PangoCairoFont"
                                   PANGO-CAIRO-FONT
@@ -41,8 +40,7 @@
           (g:gtype (cffi:foreign-funcall "pango_cairo_font_map_get_type" :size))))
   ;; Get the names of the interface properties.
   (is (equal '()
-             (mapcar #'g:param-spec-name
-                     (g:object-interface-list-properties "PangoCairoFontMap"))))
+             (list-interface-properties "PangoCairoFontMap")))
   ;; Get the interface definition
   (is (equal '(DEFINE-G-INTERFACE "PangoCairoFontMap"
                                   PANGO-CAIRO-FONT-MAP
@@ -68,40 +66,104 @@
   (is (typep (pango:cairo-font-map-new) 'pango:font-map)))
 
 ;;;     pango_cairo_font_map_new_for_font_type
+
+(test cairo-font-map-new-for-font-type
+  (is-false (pango:cairo-font-map-new-for-font-type :toy))
+  (is-true  (pango:cairo-font-map-new-for-font-type :ft))
+  (is-false (pango:cairo-font-map-new-for-font-type :win32))
+  (is-false (pango:cairo-font-map-new-for-font-type :quartz))
+  (is-false (pango:cairo-font-map-new-for-font-type :user))
+
+  (is (typep (pango:cairo-font-map-new-for-font-type :ft) 'pango:font-map)))
+
 ;;;     pango_cairo_font_map_get_font_type
+
+(test cairo-font-map-font-type
+  (is (eq :ft (pango:cairo-font-map-font-type (pango:cairo-font-map-default))))
+  (is (eq :ft (pango:cairo-font-map-font-type (pango:cairo-font-map-new))))
+  (is (eq :ft (pango:cairo-font-map-font-type (pango:cairo-font-map-default)))))
+
 ;;;     pango_cairo_font_map_set_resolution
 ;;;     pango_cairo_font_map_get_resolution
+
+(test cairo-font-map-resolution
+  (let ((fontmap (pango:cairo-font-map-default)))
+    (is (=  96.0d0 (pango:cairo-font-map-resolution fontmap)))
+    (is (= 200.0d0 (setf (pango:cairo-font-map-resolution fontmap) 200)))
+    (is (= 200.0d0 (pango:cairo-font-map-resolution fontmap)))))
+
 ;;;     pango_cairo_font_map_create_context
+
+;; TODO: Gets a pango:font-map object, but expects an pango:cairo-font-map
+;; object.
+
+#+nil
+(test cairo-font-map-create-context
+  (let ((fontmap (pango:cairo-font-map-default)))
+    (is-false (pango:cairo-font-map-create-context fontmap))))
+
 ;;;     pango_cairo_font_get_scaled_font
+
+;; TODO: The argument is a pango:font object, but how do we get it from a
+;; pango:font-map?
+
 ;;;     pango_cairo_context_set_resolution
 ;;;     pango_cairo_context_get_resolution
+
+(test cairo-context-resolution
+  (let ((context (pango:font-map-create-context (pango:cairo-font-map-default))))
+    (is (= -1.0d0 (pango:cairo-context-resolution context)))
+    (is (= 96.0d0 (setf (pango:cairo-context-resolution context) 96)))
+    (is (= 96.0d0 (pango:cairo-context-resolution context)))))
 
 ;;;     pango_cairo_context_set_font_options
 ;;;     pango_cairo_context_get_font_options
 
-;; TODO: We need the implementation of GDK-PANGO-CONTEXT-GET
-
-#+nil
 (test cairo-context-font-options
-  (let ((context (gdk-pango-context-get))
-        (options (cairo-font-options-create)))
-    (is (cairo-font-options-equal options
-                                  (setf (pango:cairo-context-font-options context)
-                                        options)))
-    (is (cairo-font-options-equal options
-                                  (pango:cairo-context-font-options context)))
-
-    (is-false (setf (pango:cairo-context-font-options context) nil))
-    (is-false (pango:cairo-context-font-options context))))
+  (let ((context (pango:font-map-create-context (pango:cairo-font-map-default)))
+        (options (cairo:font-options-create)))
+    (is-false (pango:cairo-context-font-options context))
+    (is (cffi:pointer-eq options
+                         (setf (pango:cairo-context-font-options context)
+                               options)))
+    ;; TODO: The pointers are no longer eq! Why?
+    #+nil
+    (is (cffi:pointer-eq options
+                         (pango:cairo-context-font-options context)))
+    (is-false (cairo:font-options-destroy options))))
 
 ;;;     PangoCairoShapeRendererFunc
-;;;
 ;;;     pango_cairo_context_set_shape_renderer
 ;;;     pango_cairo_context_get_shape_renderer
+
 ;;;     pango_cairo_create_context
+
+(test cairo-create-context
+  (with-cairo-context-for-image-surface (cr :rgb24 200 400)
+    (is (typep (pango:cairo-create-context cr) 'pango:context))))
+
 ;;;     pango_cairo_update_context
+
+(test cairo-update-context
+  (with-cairo-context-for-image-surface (cr :rgb24 200 400)
+    (let ((context (pango:cairo-create-context cr)))
+      (is (typep context 'pango:context))
+      (is-false (pango:cairo-update-context cr context)))))
+
 ;;;     pango_cairo_create_layout
+
+(test cairo-create-layout
+  (with-cairo-context-for-image-surface (cr :rgb24 200 400)
+    (is (typep (pango:cairo-create-layout cr) 'pango:layout))))
+
 ;;;     pango_cairo_update_layout
+
+(test cairo-update-layout
+  (with-cairo-context-for-image-surface (cr :rgb24 200 400)
+    (let ((layout (pango:cairo-create-layout cr)))
+      (is (typep layout 'pango:layout))
+      (is-false (pango:cairo-update-layout cr layout)))))
+
 ;;;     pango_cairo_show_glyph_string
 ;;;     pango_cairo_show_glyph_item
 ;;;     pango_cairo_show_layout_line
@@ -112,4 +174,4 @@
 ;;;     pango_cairo_layout_path
 ;;;     pango_cairo_error_underline_path
 
-;;; --- 2023-1-3 ---------------------------------------------------------------
+;;; --- 2023-1-17 --------------------------------------------------------------
--- test/rtest-pango.lisp
+++ test/rtest-pango.lisp
@@ -9,6 +9,7 @@
                          #:+pango-scale-large+
                          #:+pango-scale-x-large+
                          #:+pango-scale-xx-large+)
+  (:import-from :cairo   #:with-cairo-context-for-image-surface)
   (:import-from :gobject #:+g-type-boxed+
                          #:define-g-interface
                          #:define-g-enum
@@ -20,6 +21,56 @@
 (defun approx-equal (x y &optional (eps 1.0d-1))
   (or (< (abs (- x y)) eps)
       (< (abs (- x y)) (* eps (max (abs x) (abs y))))))
+
+(defun list-children (gtype)
+  (sort (mapcar #'g:type-name (g:type-children gtype))
+        #'string<))
+
+(defun list-interfaces (gtype)
+  (mapcar #'g:type-name (g:type-interfaces gtype)))
+
+;; A sorted list of the class property names without inherited properties
+(defun list-properties (gtype)
+  (sort (set-difference (mapcar #'g:param-spec-name
+                                (g:object-class-list-properties gtype))
+                        (mapcar #'g:param-spec-name
+                                (g:object-class-list-properties
+                                  (g:type-parent gtype)))
+                        :test #'string=)
+        #'string<))
+
+(defun list-interface-properties (gtype)
+  (mapcar #'g:param-spec-name
+          (g:object-interface-list-properties gtype)))
+
+;; A sorted list of the signal names of a class
+(defun list-signals (gtype)
+  (sort (mapcar #'g:signal-name
+                (g:signal-list-ids gtype)) #'string<))
+
+(defun list-flags-item-name (gtype)
+  (mapcar #'gobject:flags-item-name
+          (gobject:get-flags-items gtype)))
+
+(defun list-flags-item-nick (gtype)
+  (mapcar #'gobject:flags-item-nick
+          (gobject:get-flags-items gtype)))
+
+(defun list-flags-item-value (gtype)
+  (mapcar #'gobject:flags-item-value
+          (gobject:get-flags-items gtype)))
+
+(defun list-enum-item-name (gtype)
+  (mapcar #'gobject:enum-item-name
+          (gobject:get-enum-items gtype)))
+
+(defun list-enum-item-nick (gtype)
+  (mapcar #'gobject:enum-item-nick
+          (gobject:get-enum-items gtype)))
+
+(defun list-enum-item-value (gtype)
+  (mapcar #'gobject:enum-item-value
+          (gobject:get-enum-items gtype)))
 
 (def-suite pango-suite)
 (in-suite pango-suite)
